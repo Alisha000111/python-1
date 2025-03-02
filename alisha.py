8da49a45d0d8a320e3ffc25b64d82cf6f153e2c3
Alisha Muhammad Irfan <160208713+Alisha000111@users.noreply.github.com>
2025-03-02 15:45:38 +0500

    Add files via upload

---
+++ alisha.py
@@ -0,0 +1,57 @@
+# Python Data Types
+#  Integers
+num = 100
+print(num)
+#  Floats
+pi = 10.5
+print(pi)
+# Strings
+name = "Alisha"
+print(name)
+# Boolean
+is_student = False
+print(is_student)
+# List
+fruits = ["mango", "kiwi", "watermelon"]
+print(fruits)
+#  Tuple 
+coordinates = (30, 60)
+print(coordinates)
+#  Dictionary
+student = {"name": "Alisha", "age": 50}
+print(student)
+# Special Keywords
+# if, else, elif
+age = 20
+if age >= 18:
+    print("adult")
+else:
+    print("minor")
+# for, while
+for i in range(6):
+    print(i)  
+count = 0
+while count < 6:
+    print(count) 
+    count += 1
+# def
+def greet(name):
+    print(f"Hello, {name}!")
+greet("Alisha") 
+# return
+def add(a, b):
+    return a + b
+result = add(6, 12)
+print(result)
+# import
+import math
+print(math.sqrt(20))
+# None
+x = None
+print(x)
+# break, continue
+for i in range(10):
+    if i == 5:
+        break 
+    print(i)
+
